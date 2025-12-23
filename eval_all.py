751e1879049ae9892ccf61620a2c11ad35b7d964
hungtrab <hungtranbg1704@gmail.com>
2025-12-24 01:10:05 +0700

    fd

--- eval_all.py
+++ eval_all.py
@@ -218,7 +218,7 @@ def evaluate_run(run_path, run_name, device='cuda'):
         )
         
         # Save results to text file
-        txt_path = os.path.join(run_path, str(run_path.name),'evaluation_results.txt')
+        txt_path = os.path.join(run_path, 'evaluation_results.txt')
         with open(txt_path, 'w') as f:
             f.write(f"Evaluation Results for {run_name}\n")
             f.write(f"{'='*80}\n\n")
